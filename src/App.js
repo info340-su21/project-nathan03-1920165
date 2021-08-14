a9df8af40da64c108b9e9d6a9b684349962c79d0
Nathaniel Sy Su <nathan03@uw.edu>
2021-08-13 17:59:42 -0700

    Edit input tag

--- src/App.js
+++ src/App.js
@@ -149,10 +149,10 @@ function LoginPage() {
 
                     <div className="login-info">
                         <label for="uname">Username:</label>
-                        <input type="text" id="uname" name="uname" required></input>
+                        <input type="text" id="uname" name="uname" required />
 
                         <label for="pwd">Password:</label>
-                        <input type="password" id="pwd" name="pwd" required></input>
+                        <input type="password" id="pwd" name="pwd" required />
                         <p>(case sensitive)</p>
 
                         <button><Link exact to='/home'>LOG IN</Link></button>
@@ -194,7 +194,7 @@ function LoginPage() {
     )
 } */}
 
-function CreateUserPage() {
+function CreateUserPage(props) {
     // let registerLabels = [
     //     {ID: 'fname', Label: 'First Name:'},
     //     {ID: 'lname', Label: 'Last Name:'},
@@ -232,6 +232,31 @@ function CreateUserPage() {
         let copy = {...formValues};
         copy[field] = value;
         setFormValues(copy);
+    }
+
+    //handle createAccount button
+    const handleCreateAccount = (event) => {
+        event.preventDefault();
+        props.signUpCallback(
+            formValues.firstname,
+            formValues.lastname,
+            formValues.username,
+            formValues.password,
+            formValues.picture,
+            formValues.pronouns,
+            formValues.city,
+            formValues.state,
+            formValues.country,
+            formValues.primary_lang,
+            formValues.second_lang,
+            formValues.phone,
+            formValues.email,
+            formValues.facebook,
+            formValues.instagram,
+            formValues.snapchat,
+            formValues.wechat,
+            formValues.discord
+        )
     }
 
     return (
@@ -490,8 +515,8 @@ function FilterForm() {
             </form>
 
             <form>
-                <input type="submit" value="Submit Form" name="submit"></input>
-                <input type="reset" value="Reset Form" name="reset"></input>
+                <input type="submit" value="Submit Form" name="submit" />
+                <input type="reset" value="Reset Form" name="reset" />
             </form>
         </div>
     )
@@ -784,19 +809,19 @@ function AccountPage() {
                     <h2>Account Information</h2>
                     <div>
                         <label for="fname_field">First Name:</label>
-                        <input type="text" id="fname_field" name="fname" required></input>
+                        <input type="text" id="fname_field" name="fname" required />
                     </div>
                     <div>
                         <label for="lname_field">Last Name:</label>
-                        <input type="text" id="lname_field" name="lname" required></input>
+                        <input type="text" id="lname_field" name="lname" required />
                     </div>
                     <div>
                         <label for="uname_field">Username:</label>
-                        <input type="text" id="uname_field" name="uname" pattern=".{8,}" placeholder="8 character requirement" required></input>
+                        <input type="text" id="uname_field" name="uname" pattern=".{8,}" placeholder="8 character requirement" required />
                     </div>
                     <div>
                         <label for="pwd_field">Password:</label>
-                        <input type="password" id="pwd_field" name="pwd" pattern=".{8,}" placeholder="8 character requirement" required></input>
+                        <input type="password" id="pwd_field" name="pwd" pattern=".{8,}" placeholder="8 character requirement" required />
                     </div>
                 </form>
             </section>
@@ -807,7 +832,7 @@ function AccountPage() {
                     <em>Tell us all about you.</em>
                     <div>
                         <label for="picture_upload">Choose a profile picture:</label>
-                        <input type="file" id="picture_upload" name="picture" accept=".png, .jpg, .jpeg"></input>
+                        <input type="file" id="picture_upload" name="picture" accept=".png, .jpg, .jpeg" />
                     </div>
                     <div>
                         <label for="pronouns_dropdown">Personal Pronoun:</label>
@@ -820,23 +845,23 @@ function AccountPage() {
                     </div>
                     <div>
                         <label for="city_field">Hometown City:</label>
-                        <input type="text" id="city_field" name="city" placeholder="Seattle" required></input>
+                        <input type="text" id="city_field" name="city" placeholder="Seattle" required />
                     </div>
                     <div>
                         <label for="state_field">Hometown State:</label>
-                        <input type="text" id="state_field" name="state" placeholder="WA" required></input>
+                        <input type="text" id="state_field" name="state" placeholder="WA" required />
                     </div>
                     <div>
                         <label for="country_field">Hometown Country:</label>
-                        <input type="text" id="country_field" name="country" placeholder="USA" required></input>
+                        <input type="text" id="country_field" name="country" placeholder="USA" required />
                     </div>
                     <div>
                         <label for="primary_lang_field">Primary Language:</label>
-                        <input type="text" id="primary_lang_field" name="primary_lang" placeholder="English" required></input>
+                        <input type="text" id="primary_lang_field" name="primary_lang" placeholder="English" required />
                     </div>
                     <div>
                         <label for="second_lang_field">Secondary Language:</label>
-                        <input type="text" id="second_lang_field" name="second_lang" placeholder="Chinese" required></input>
+                        <input type="text" id="second_lang_field" name="second_lang" placeholder="Chinese" required />
                     </div>
                 </form>
             </section>
@@ -847,31 +872,31 @@ function AccountPage() {
                     <em>Provide your contact information to be reached by other students.</em>
                     <div>
                         <label for="phone">Phone Number:</label>
-                        <input type="tel" id="phone" name="phone" placeholder="206-543-2100" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"></input>
+                        <input type="tel" id="phone" name="phone" placeholder="206-543-2100" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" />
                     </div>
                     <div>
                         <label for="email">Email Address:</label>
-                        <input type="email" id="email" name="email" placeholder="example@domain.com"></input>
+                        <input type="email" id="email" name="email" placeholder="example@domain.com" />
                     </div>
                     <div>
                         <label for="facebook"><i className="fab fa-facebook" aria-label="Facebook"></i> Facebook: </label>
-                        <input type="text" id="facebook" name="facebook"></input>
+                        <input type="text" id="facebook" name="facebook" />
                     </div>
                     <div>
                         <label for="instagram"><i className="fab fa-instagram" aria-label="Instagram"></i> Instagram: </label>
-                        <input type="text" id="instagram" name="instagram" placeholder="@handle"></input>
+                        <input type="text" id="instagram" name="instagram" placeholder="@handle" />
                     </div>
                     <div>
                         <label for="snapchat"><i className="fab fa-snapchat" aria-label="Snapchat"></i> Snapchat: </label>
-                        <input type="text" id="snapchat" name="snapchat" placeholder="@handle"></input>
+                        <input type="text" id="snapchat" name="snapchat" placeholder="@handle" />
                     </div>
                     <div>
                         <label for="wechat"><i className="fab fa-weixin" aria-label="WeChat"></i> WeChat: </label>
-                        <input type="text" id="wechat" name="wechat"></input>
+                        <input type="text" id="wechat" name="wechat" />
                     </div>
                     <div>
                         <label for="discord"><i className="fab fa-discord" aria-label="Discord"></i> Discord: </label>
-                        <input type="text" id="discord" name="discord" placeholder="@discord#0000"></input>
+                        <input type="text" id="discord" name="discord" placeholder="@discord#0000" />
                     </div>
                 </form>
             </section>
@@ -882,7 +907,7 @@ function AccountPage() {
                 <form>
                     <div>
                         <label for="major">(Intended) Major:</label>
-                        <input type="text" id="major" name="major" placeholder="Informatics"></input>
+                        <input type="text" id="major" name="major" placeholder="Informatics" />
                     </div>
                     <div>
                         <label for="class-standing">Class Standing</label>
@@ -897,7 +922,7 @@ function AccountPage() {
                     </div>
                     <div>
                         <label for="graduation">Expected Graduation Month and Year:</label>
-                        <input type="month" id="graduation" name="graduation"></input>
+                        <input type="month" id="graduation" name="graduation" />
                     </div>
                 </form>
             </section>
@@ -1127,7 +1152,7 @@ function AccountPage() {
                     </div>
                     <div>
                         <label for="adjective_field">What are three adjectives that describe you?</label>
-                        <input type="text" id="adjective_field" name="adjective" placeholder="adj1, adj2, adj3"></input>
+                        <input type="text" id="adjective_field" name="adjective" placeholder="adj1, adj2, adj3" />
                     </div>
                     <div>
                         <label for="social_rank_dropdown">How social are you?</label>
@@ -1144,56 +1169,56 @@ function AccountPage() {
                     <h3>Music</h3>
                     <div>
                         <label for="music_field">What music genres do you listen to?</label>
-                        <input type="text" id="music_field" name="music"></input>
+                        <input type="text" id="music_field" name="music" />
                     </div>
                     <div>
                         <label for="artist_field">Who is your favorite artist?</label>
-                        <input type="text" id="artist_field" name="artist"></input>
+                        <input type="text" id="artist_field" name="artist" />
                     </div>
 
                     <h3>Movies</h3>
                     <div>
                         <label for="movie_field">What movie genres do you like to watch?</label>
-                        <input type="text" id="movie_field" name="movie"></input>
+                        <input type="text" id="movie_field" name="movie" />
                     </div>
                     <div>
                         <label for="fav_movie_field">What is your favorite movie of all time?</label>
-                        <input type="text" id="fav_movie_field" name="favorite_movie"></input>
+                        <input type="text" id="fav_movie_field" name="favorite_movie" />
                     </div>
 
                     <h3>TV Show/Series</h3>
                     <div>
                         <label for="series_field">What TV show/series is your favorite?</label>
-                        <input type="text" id="series_field" name="movie"></input>
+                        <input type="text" id="series_field" name="movie" />
                     </div>
                     <div className="streaming">
                         <label for="streaming">If you had to pick one streaming service to binge watch, which would you choose?</label>
                         <div>
-                            <input type="radio" id="netflix_radio" name="streaming" value="Netflix"></input>
+                            <input type="radio" id="netflix_radio" name="streaming" value="Netflix" />
                             <label for="netflix_radio">Netflix</label>
                         </div>
                         <div>
-                            <input type="radio" id="hulu_radio" name="streaming" value="Hulu"></input>
+                            <input type="radio" id="hulu_radio" name="streaming" value="Hulu" />
                             <label for="hulu_radio">Hulu</label>
                         </div>
                         <div>
-                            <input type="radio" id="disneyplus_radio" name="streaming" value="Disney+"></input>
+                            <input type="radio" id="disneyplus_radio" name="streaming" value="Disney+" />
                             <label for="disneyplus_radio">Disney+</label>
                         </div>
                         <div>
-                            <input type="radio" id="hbomax_radio" name="streaming" value="HBO Max"></input>
+                            <input type="radio" id="hbomax_radio" name="streaming" value="HBO Max" />
                             <label for="hbomax_radio">HBO Max</label>
                         </div>
                         <div>
-                            <input type="radio" id="primevideo_radio" name="streaming" value="Amazon Prime Video"></input>
+                            <input type="radio" id="primevideo_radio" name="streaming" value="Amazon Prime Video" />
                             <label for="primevideo_radio">Amazon Prime Video</label>
                         </div>
                         <div>
-                            <input type="radio" id="peacock_radio" name="streaming" value="Peacock"></input>
+                            <input type="radio" id="peacock_radio" name="streaming" value="Peacock" />
                             <label for="peacock_radio">Peacock</label>
                         </div>
                         <div>
-                            <input type="radio" id="none_radio" name="streaming" value="None of the above"></input>
+                            <input type="radio" id="none_radio" name="streaming" value="None of the above" />
                             <label for="none_radio">I am not subscribed to any streaming service</label>
                         </div>
                     </div>
@@ -1214,8 +1239,8 @@ function AccountPage() {
             <section className="submit">
                 <h2>Complete Questionnaire</h2>
                 <form>
-                    <input type="submit" value="Submit Form" name="submit"></input>
-                    <input type="reset" value="Reset Form" name="reset"></input>
+                    <input type="submit" value="Submit Form" name="submit" />
+                    <input type="reset" value="Reset Form" name="reset" />
                 </form>
             </section>
         </main>
