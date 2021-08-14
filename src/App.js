fb2ffd392c2e77cd82a54d2dda6ccdac3dade6fa
Nathaniel Sy Su <nathan03@uw.edu>
2021-08-13 17:39:20 -0700

    Modify CreateUserPage component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink, Redirect, useParams } from 'react-router-dom';
 
 import mcmahon from './img/mcmahon-room.jpg';
@@ -41,12 +41,14 @@ function App() {
                 </Route>
             </Switch>
 
-            <footer>
-                <address>
-                    Contact us at <a href="mailto:findahusky@uw.edu">findahusky@uw.edu</a>, or at <a href="tel:206-543-2100">(206) 543-2100</a>
-                </address>
-                <p>&copy; 2021 Juliane De Los Santos & Nathaniel Sy Su</p>
-            </footer>
+            <div className="footer-container">
+                <footer>
+                    <address>
+                        Contact us at <a href="mailto:findahusky@uw.edu">findahusky@uw.edu</a>, or at <a href="tel:206-543-2100">(206) 543-2100</a>
+                    </address>
+                    <p>&copy; 2021 Juliane De Los Santos & Nathaniel Sy Su</p>
+                </footer>
+            </div>
         </BrowserRouter>
     )
 }
@@ -166,27 +168,95 @@ function LoginPage() {
     )
 }
 
+{/* function FormSection(props) { //array of objects (input field names)
+    let labelsArray = props.arrayOfObj;
+
+    const [inputValue, setInputtedValue] = useState('');
+    const handleChange = (event) => {
+        let newValue = event.target.value
+        setInputtedValue(newValue);
+    }
+
+    let elementsArray = labelsArray.map((SingleObj) => {
+        let transformedValue = (
+            <div>
+                <label for={SingleObj.ID}>{SingleObj.Label}</label>
+                <input type="text" onChange={handleChange} value={inputValue} id={SingleObj.ID} name={SingleObj.ID} required />
+            </div>
+        )
+        return transformedValue;
+    })
+
+    return (
+        <div>
+            {elementsArray}
+        </div>
+    )
+} */}
+
 function CreateUserPage() {
+    // let registerLabels = [
+    //     {ID: 'fname', Label: 'First Name:'},
+    //     {ID: 'lname', Label: 'Last Name:'},
+    //     {ID: 'uname', Label: 'Username:'},
+    //     {ID: 'pwd', Label: 'Password:'}
+    // ]
+
+    //stores all form values in a single object
+    const [formValues, setFormValues] = useState({
+        'firstname': undefined,
+        'lastname': undefined,
+        'username': undefined,
+        'password': undefined,
+        'picture': undefined,
+        'pronouns': undefined,
+        'city': undefined,
+        'state': undefined,
+        'country': undefined,
+        'primary_lang': undefined,
+        'second_lang': undefined,
+        'phone': undefined,
+        'email': undefined,
+        'facebook': undefined,
+        'instagram': undefined,
+        'snapchat': undefined,
+        'wechat': undefined,
+        'discord': undefined
+    })
+
+    //update state for a specific field
+    const handleChange = (event) => {
+        let field = event.target.name;
+        let value = event.target.value;
+
+        let copy = {...formValues};
+        copy[field] = value;
+        setFormValues(copy);
+    }
+
     return (
         <main className="main-createUser">
             <section className="profile">
                 <form className="createUser-text">
                     <h2 className="createUser-text">Create an Account</h2>
+
+                    {/* <FormSection arrayOfObj={registerLabels} /> */}
+                
                     <div>
                         <label for="fname_field">First Name:</label>
-                        <input type="text" id="fname_field" name="fname" required></input>
+                        <input type="text" id="fname_field" name="firstname" onChange={handleChange} required />
                     </div>
                     <div>
                         <label for="lname_field">Last Name:</label>
-                        <input type="text" id="lname_field" name="lname" required></input>
+                        <input type="text" id="lname_field" name="lastname" onChange={handleChange} required />
                     </div>
                     <div>
                         <label for="uname_field">Username:</label>
-                        <input type="text" id="uname_field" name="uname" pattern=".{8,}" placeholder="8 character requirement" required></input>
+                        <input type="text" id="uname_field" name="username" pattern=".{8,}" placeholder="8 character requirement" onChange={handleChange} required />
                     </div>
                     <div>
                         <label for="pwd_field">Password:</label>
-                        <input type="password" id="pwd_field" name="pwd" pattern=".{8,}" placeholder="8 character requirement" required></input>
+                        <input type="password" id="pwd_field" name="password" pattern=".{8,}" placeholder="8 character requirement" onChange={handleChange} required />
                     </div>
                 </form>
             </section>
@@ -197,11 +267,11 @@ function CreateUserPage() {
                     <em>Tell us all about you.</em>
                     <div>
                         <label for="picture_upload">Choose a profile picture:</label>
-                        <input type="file" id="picture_upload" name="picture" accept=".png, .jpg, .jpeg"></input>
+                        <input type="file" id="picture_upload" name="picture" onChange={handleChange} accept=".png, .jpg, .jpeg" />
                     </div>
                     <div>
                         <label for="pronouns_dropdown">Personal Pronoun:</label>
-                        <select id="pronouns_dropdown" name="pronouns">
+                        <select id="pronouns_dropdown" name="pronouns" onChange={handleChange}>
                             <option hidden="">Select one</option>
                             <option value="she">she/her/hers</option>
                             <option value="he">he/him/his</option>
@@ -210,23 +280,23 @@ function CreateUserPage() {
                     </div>
                     <div>
                         <label for="city_field">Hometown City:</label>
-                        <input type="text" id="city_field" name="city" placeholder="Seattle" required></input>
+                        <input type="text" id="city_field" name="city" onChange={handleChange} placeholder="Seattle" required />
                     </div>
                     <div>
                         <label for="state_field">Hometown State:</label>
-                        <input type="text" id="state_field" name="state" placeholder="WA" required></input>
+                        <input type="text" id="state_field" name="state" onChange={handleChange} placeholder="WA" required />
                     </div>
                     <div>
                         <label for="country_field">Hometown Country:</label>
-                        <input type="text" id="country_field" name="country" placeholder="USA" required></input>
+                        <input type="text" id="country_field" name="country" onChange={handleChange} placeholder="USA" required />
                     </div>
                     <div>
                         <label for="primary_lang_field">Primary Language:</label>
-                        <input type="text" id="primary_lang_field" name="primary_lang" placeholder="English" required></input>
+                        <input type="text" id="primary_lang_field" name="primary_lang" onChange={handleChange} placeholder="English" required />
                     </div>
                     <div>
                         <label for="second_lang_field">Secondary Language:</label>
-                        <input type="text" id="second_lang_field" name="second_lang" placeholder="Chinese" required></input>
+                        <input type="text" id="second_lang_field" name="second_lang" onChange={handleChange} placeholder="Chinese" required />
                     </div>
                 </form>
             </section>
@@ -237,31 +307,31 @@ function CreateUserPage() {
                     <em>Provide your contact information to be reached by other students.</em>
                     <div>
                         <label for="phone">Phone Number:</label>
-                        <input type="tel" id="phone" name="phone" placeholder="206-543-2100" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"></input>
+                        <input type="tel" id="phone" name="phone" onChange={handleChange} placeholder="206-543-2100" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
                     </div>
                     <div>
                         <label for="email">Email Address:</label>
-                        <input type="email" id="email" name="email" placeholder="example@domain.com"></input>
+                        <input type="email" id="email" name="email" onChange={handleChange} placeholder="example@domain.com" required />
                     </div>
                     <div>
                         <label for="facebook"><i className="fab fa-facebook" aria-label="Facebook"></i> Facebook: </label>
-                        <input type="text" id="facebook" name="facebook"></input>
+                        <input type="text" id="facebook" name="facebook" onChange={handleChange} />
                     </div>
                     <div>
                         <label for="instagram"><i className="fab fa-instagram" aria-label="Instagram"></i> Instagram: </label>
-                        <input type="text" id="instagram" name="instagram" placeholder="@handle"></input>
+                        <input type="text" id="instagram" name="instagram" onChange={handleChange} placeholder="@handle" />
                     </div>
                     <div>
                         <label for="snapchat"><i className="fab fa-snapchat" aria-label="Snapchat"></i> Snapchat: </label>
-                        <input type="text" id="snapchat" name="snapchat" placeholder="@handle"></input>
+                        <input type="text" id="snapchat" name="snapchat" onChange={handleChange} placeholder="@handle" />
                     </div>
                     <div>
                         <label for="wechat"><i className="fab fa-weixin" aria-label="WeChat"></i> WeChat: </label>
-                        <input type="text" id="wechat" name="wechat"></input>
+                        <input type="text" id="wechat" onChange={handleChange} name="wechat" />
                     </div>
                     <div>
                         <label for="discord"><i className="fab fa-discord" aria-label="Discord"></i> Discord: </label>
-                        <input type="text" id="discord" name="discord" placeholder="@discord#0000"></input>
+                        <input type="text" id="discord" name="discord" onChange={handleChange} placeholder="@discord#0000" />
                     </div>
                 </form>
             </section>
